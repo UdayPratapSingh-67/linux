ff84e8a032d2f95eba115b0fb91a9107014704bc
UdayPratapSingh-67 <vanshrajpoot629@gmail.com>
2026-03-31 23:19:12 +0530

    Update script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 5: Open Source Manifesto Generator
-# Author: RAHUL| Reg No: 24BCE10973
+# Author: UDAY| Reg No: 24BAI10919
 # Course: Open Source Software | OSS NGMC Capstone Project
 # Description: Interactively asks the user three questions
 #              and composes a personalised open-source
